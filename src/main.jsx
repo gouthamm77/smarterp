d8810467e6b0198ecc19ea1b698a9206f4cf14ff
gouthamm77 <2400031906@kluniversity.in>
2025-11-04 15:29:27 +0530

    Rename import for Login component to match case

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './login.jsx'
+import Login from './Login.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
